5e7db4472dd1b9e8f56450557b844e88e1b80091
Good1Cheese <2916675@gmail.com>
2023-11-16 23:49:33 +0300

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+	#custom aliases
+	alias fol='nautilus .'
+	alias nconf='cd ~/.config/nvim'
+	alias prop='cd ~/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/gamemodes/cstrike'
+	alias proj='cd ~/Projects'
+	alias qq='neovide.AppImage'
+	alias stdock='sudo docker-compose -f ~/Apps/devilbox/docker-compose.yml up -d'
+	alias kldock='sudo docker-compose -f ~/Apps/devilbox/docker-compose.yml kill'
+
+	#custom path vars
+	#export PATH="/home/pashkevich/.config/lsp/lua-language-server/bin:$PATH"
+end
