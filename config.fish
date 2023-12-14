6b99d72452e80652c66507e36047f8a081ec4984
Good1Cheese <2916675@gmail.com>
2023-12-14 23:36:33 +0300

    :inbox_tray: Updated configs

--- config.fish
+++ config.fish
@@ -4,13 +4,19 @@ if status is-interactive
 	alias nconf='cd ~/.config/nvim'
 	alias prop='cd ~/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/gamemodes/cstrike'
 	alias proj='cd ~/Projects'
-	alias qq='neovide.AppImage'
+	alias aws='cd ~/.config/awesome'
+	alias qq='neovide'
+	alias sudo='doas'
+	alias pac='doas pacman'
 	alias stdock='sudo docker-compose -f ~/Apps/devilbox/docker-compose.yml up -d'
 	alias kldock='sudo docker-compose -f ~/Apps/devilbox/docker-compose.yml kill'
-
-	#custom path vars
-	#export PATH="/home/pashkevich/.config/lsp/lua-language-server/bin:$PATH"
 end
+
+set -gx BSPWM "$HOME/.config/bspwm"
+# $HOME/.config/bspwm/launchers
+set -gx BSPWM_LAUNCHERS "$BSPWM/launchers"
+# $HOME/.config/bspwm/configs
+set -gx BSPWM_CONFIGS "$BSPWM/configs"
 
 function bind_bang
     switch (commandline -t)[-1]
